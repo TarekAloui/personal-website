5c6ff997801d7dbb6b19413ff5879b825e695af2
Tarek Aloui <taloui@college.harvard.edu>
2023-08-28 12:30:10 -0400

    added updates

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd4a1144bfbc244eb8ac52329568eebce',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'tarekaloui',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Tarek Aloui',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Tarek Aloui',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Tarek Aloui's portfolio and blog",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'TarekAloui21',
+  github: 'TarekAloui',
+  linkedin: 'tarek-aloui',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'https://i.postimg.cc/3xb5VSYY/profile.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -42,24 +42,40 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/about': 'd4a1144bfbc244eb8ac52329568eebce',
+    '/projects': '0d7972919e3249d8aa3086359cec77ea',
+    '/notes': '11e66bbb35914f98b07f1f88b8d6b70f',
+    '/blogs': 'c0eb23dedef64a0a946e32271b2b7258',
+    '/papers': '7c4db8134d634c229a044c69cef8b8c9',
+  },
+  //pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //nimport { author } from './lib/config';
+  //navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'd4a1144bfbc244eb8ac52329568eebce'
+    },
+    {
+      title: 'Projects',
+      pageId: '0d7972919e3249d8aa3086359cec77ea'
+    },
+    {
+      title: 'Notes',
+      pageId: '11e66bbb35914f98b07f1f88b8d6b70f'
+    },
+    {
+      title: 'Blogs',
+      pageId: 'c0eb23dedef64a0a946e32271b2b7258'
+    }, 
+    {
+      title: 'Papers',
+      pageId: '7c4db8134d634c229a044c69cef8b8c9'
+    },
+  ]
 })
