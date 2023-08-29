12e9323bce6067f2efe2404c9d13fc5a26b20b6d
Tarek Aloui <taloui@college.harvard.edu>
2023-08-29 05:41:24 -0400

    cleaning logs

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -211,13 +211,15 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
   const title = getBlockTitle(block, recordMap) || site.name
 
-  console.log('notion page', {
-    isDev: config.isDev,
-    title,
-    pageId,
-    rootNotionPageId: site.rootNotionPageId,
-    recordMap
-  })
+  if (config.isDev) {
+    console.log('notion page', {
+      isDev: config.isDev,
+      title,
+      pageId,
+      rootNotionPageId: site.rootNotionPageId,
+      recordMap
+    })
+  }
 
   if (!config.isServer) {
     // add important objects to the window global for easy debugging
